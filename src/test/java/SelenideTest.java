e20d897f498a061de5327812a6874be00e9c983d
Яроцкий Алексей Владимирович <aleksey.yarozkiy@yandex.ru>
2022-08-27 01:12:32 +0300

    fix

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -10,7 +10,7 @@ public class SelenideTest {
     @Test
     void shouldTest1() {
         open("http://localhost:9999");
-        $("[data-test-id=name] input").setValue("Алексей");
+        $("[data-test-id=name] input").setValue("Алексей Алексеев-Алексеевич");
         $("[data-test-id=phone] input").setValue("+79999999999");
         $("[data-test-id=agreement]").click();
         $("button").click();
@@ -32,7 +32,7 @@ public class SelenideTest {
         $("[data-test-id=phone] input").setValue("+791117841");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
@@ -42,7 +42,7 @@ public class SelenideTest {
         $("[data-test-id=phone] input").setValue("+79111784110");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
