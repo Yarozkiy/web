4532a704b7a41993414c83a96af42906aa78ddd1
Яроцкий Алексей Владимирович <aleksey.yarozkiy@yandex.ru>
2022-08-27 16:45:01 +0300

    Add BeforeEach

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -1,15 +1,20 @@
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.BeforeEach;
 import static com.codeborne.selenide.Condition.cssClass;
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
+
 public class SelenideTest {
+
+    @BeforeEach
+    public void setup() {
+        open("http://localhost:9999");
+    }
 
     @Test
     void shouldTest1() {
-        open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Алексей Алексеев-Алексеевич");
         $("[data-test-id=phone] input").setValue("+79999999999");
         $("[data-test-id=agreement]").click();
@@ -18,16 +23,14 @@ public class SelenideTest {
     }
     @Test
     void shouldTestNotClickAgreement() {
-        open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Алексей");
         $("[data-test-id=phone] input").setValue("+79111784110");
-        //  $("[data-test-id=agreement]").click();
+        $("[data-test-id=agreement]").click();
         $("button").click();
         $("[data-test-id=agreement]").shouldHave(cssClass("input_invalid"));
     }
     @Test
     void shouldTestInvalidNumber() {
-        open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Алексей");
         $("[data-test-id=phone] input").setValue("+791117841");
         $("[data-test-id=agreement]").click();
@@ -37,7 +40,6 @@ public class SelenideTest {
 
     @Test
     void shouldTestInvalidName() {
-        open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Aleksey");
         $("[data-test-id=phone] input").setValue("+79111784110");
         $("[data-test-id=agreement]").click();
@@ -47,22 +49,20 @@ public class SelenideTest {
 
     @Test
     void shouldTestEmptyName() {
-        open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("");
         $("[data-test-id=phone] input").setValue("+79111784110");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        $("[data-test-id=name] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
     void shouldTestEmptyNumber() {
-        open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Алексей");
         $("[data-test-id=phone] input").setValue("");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
 
